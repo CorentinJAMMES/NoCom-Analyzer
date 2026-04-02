771733937d98f76ec098f7c3e87a3d43e76cb335
arblakmin <cjammes50@gmail.com>
2026-04-02 17:43:17 +0200

    amélioration du système (ajout de + de code de detection et on ignore les codes d'erreurs)

--- src/nocom.py
+++ src/nocom.py
@@ -9,7 +9,6 @@ with open("../ressources/wordlist.txt", "r") as fichier:
         # .strip() sert à enlever le retour à la ligne invisible (\n) à la fin du mot
         mot = ligne.strip() 
         url_temp= f"{url_cible}/{mot}"  # On construit l'URL à tester
-        print(f"[*] Lancement du scan sur : {url_temp}")
 
         try:
             reponse = requests.get(url_temp)
@@ -17,8 +16,12 @@ with open("../ressources/wordlist.txt", "r") as fichier:
             # On affiche le résultat
             if reponse.status_code == 200:
                 print(f"[+] Succès ! Le site a répondu : {reponse.status_code} OK, URL : {url_temp}")
-            else:
-                print(f"[-] Statut différent : {reponse.status_code}")
+            elif reponse.status_code == 403:
+                print(f"[~] Accès non autorisé : {reponse.status_code}, URL : {url_temp}")
+            elif reponse.status_code == 301 or reponse.status_code == 302:
+                print(f"[~] Redirection détectée : {reponse.status_code}, URL : {url_temp}")
+            elif reponse.status_code == 204:
+                print(f"[~] Pas de contenu : {reponse.status_code}, URL : {url_temp}")
 
         except Exception as e:
             print(f"[!] Erreur de connexion : {e}")
